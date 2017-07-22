339f8fbca06aa4b735a6ae8abad9bf7659d72490
Ryan Canavan <rycanavan@gmail.com>
2017-07-22 16:09:01 -0400

    Updated README and set up basic weather card

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,17 +9,23 @@ import 'rxjs/add/operator/map';
 })
 export class HomePage {
   
-  image: any;
+  icon_url: any;
+  weather: any;
   temp: any;
   feelslike: any;
+  city: any;
+  time: any;
 
   constructor(public navCtrl: NavController, public http: Http) {
 
-    this.http.get('http://api.wunderground.com/api/5b4386802cb6e185/conditions/q/CA/San_Francisco.json').map(res => res.json())
+    this.http.get('http://api.wunderground.com/api/5b4386802cb6e185/conditions/q/19104.json').map(res => res.json())
       .subscribe(data => {
-        this.image = data.current_observation.icon_url;
-        this.temp = data.current_observation.temp_f;
-        this.feelslike = data.current_observation.feelslike_f;
+        this.icon_url = data.current_observation.icon_url;
+        this.weather = data.current_observation.weather;
+        this.temp = parseInt(data.current_observation.temp_f);
+        this.feelslike = parseInt(data.current_observation.feelslike_f);
+        this.city = data.current_observation.display_location.full;
+        this.time = data.current_observation.observation_time;
       },
       err => {
         console.log("ERROR!");
